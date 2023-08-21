622e6f9898f425170ee5d44e6de766c961a4899c
Anthony Wood <apwood@uh.edu>
2023-08-21 12:19:57 -0500

    docstring

--- flux_tool/config.py
+++ flux_tool/config.py
@@ -10,6 +10,42 @@ import numpy as np
 
 
 class AnalysisConfig:
+    """
+    Configuration class for analysis parameters and paths.
+
+    This class encapsulates the configuration settings and paths required
+    for performing analysis. It allows setting up binning, paths for sources,
+    results, plots, and handling ignored histogram names. Additionally, it
+    provides methods for parsing filenames, iterating through samples, and
+    loading configuration from strings or files.
+
+    Parameters:
+    project_config (dict): A dictionary containing the project configuration
+        settings, including plotting options, binning, paths, and more.
+
+    Attributes:
+    bin_edges (dict): A dictionary containing the bin edges for different
+        neutrino types.
+    neutrinos (list): A list of neutrino types: ["nue", "nuebar", "numu", "numubar"].
+    nominal_samples (dict): A dictionary containing paths to nominal samples for
+        "fhc" and "rhc" horns.
+    output_file_name (str): The name of the output file.
+    plot_opts (dict): Plotting options, such as x-axis limits.
+    plots_path (Path): Path to the directory where plots will be saved.
+    ppfx (dict): Configuration for the PPFX.
+    products_file (str): Path to the products file.
+    results_path (Path): Path to the directory where analysis results will be saved.
+    sources_path (Path): Path to the directory containing analysis sources.
+
+    Methods:
+    verify_paths(): Verifies the existence of necessary paths and creates them if missing.
+    ignored_histogram_names: Generator yielding ignored histogram names based on PPFX settings.
+    ignored_hist_filter(hist_name: str) -> bool: Checks if a histogram name should be ignored.
+    parse_filename(name: str) -> tuple[str, int]: Parses a filename to extract horn and run ID.
+    itersamples(): Iterates through source files, yielding file, horn, and run ID.
+    from_str(config_str: str) -> AnalysisConfig: Creates an instance from a configuration string.
+    from_file(config_file: str) -> AnalysisConfig: Creates an instance from a configuration file.
+    """
     __slots__ = (
         "bin_edges",
         "neutrinos",
