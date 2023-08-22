0d2c00d49691139c96326a923bb81447f717e4ea
Anthony Wood <apwood@uh.edu>
2023-08-22 14:33:14 -0500

    added progress bar

--- flux_tool/preprocessor.py
+++ flux_tool/preprocessor.py
@@ -3,9 +3,13 @@ from concurrent.futures import ProcessPoolExecutor as Executor
 from functools import partial
 
 import pandas as pd
+from rich.progress import Progress
 
 from flux_tool.config import AnalysisConfig
 from flux_tool.normalize_and_rebin_data import normalize_flux_to_pot
+
+# from tqdm import tqdm
+
 
 
 class Preprocessor:
@@ -14,19 +18,24 @@ class Preprocessor:
     def __init__(self, cfg: AnalysisConfig) -> None:
         logging.info("\n=============== BEGINNING PREPROCESSING ===============")
         logging.info(
-            "Reading input files, normalized to POT, and rebinning, if necessary. This might take a while..."
+            "Reading input files, normalized to POT, and rebinning, if necessary"
         )
-        with Executor() as executor:
-            res = executor.map(
-                partial(
-                    normalize_flux_to_pot,
-                    bin_edges=cfg.bin_edges,
-                    hist_name_filter=cfg.ignored_hist_filter,
-                ),
-                *zip(*list(cfg.itersamples())),
-            )
+        jobs = list(cfg.itersamples())
+        fn = partial(
+            normalize_flux_to_pot,
+            bin_edges=cfg.bin_edges,
+            hist_name_filter=cfg.ignored_hist_filter,
+        )
+        results = []
+        with Progress() as progress:
+            task_id = progress.add_task("[cyan]Working...", total=len(jobs))
+            with Executor() as executor:
+                for job in jobs:
+                    future = executor.submit(fn, *job)
+                    future.add_done_callback(lambda _: progress.advance(task_id))
+                    results.append(future.result())
 
-        df = pd.concat(res)
+        df = pd.concat(results)
         self.nominal_flux_df = df.loc[df["universe"].isna()].drop("universe", axis=1)
         self.ppfx_correction_df = df.loc[
             (df["run_id"] == 15) & (df["universe"].notna())
