759fd75672faf23a4f237d09b9c909a0123dbeff
Anthony Wood <apwood@uh.edu>
2023-10-24 15:40:50 -0500

    add additional plots

--- flux_tool/vis_scripts/__init__.py
+++ flux_tool/vis_scripts/__init__.py
@@ -8,6 +8,6 @@ from flux_tool.vis_scripts.fractional_uncertainties import (
     plot_hadron_fractional_uncertainties_mesinc_breakout, plot_uncertainties)
 from flux_tool.vis_scripts.parent_spectra import plot_parents
 from flux_tool.vis_scripts.pca_plots import (
-    pca_uncertainty_comparison, plot_hadron_systs_and_pca_variances,
-    plot_pca_systematic_shifts, scree_plot)
+    pca_mesinc_overlay, plot_hadron_systs_and_pca_variances,
+    plot_pca_systematic_shifts, plot_top_principal_components, scree_plot)
 from flux_tool.vis_scripts.ppfx_universes import plot_ppfx_universes
